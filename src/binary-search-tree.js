35f51cdbaf0b519bbe1d169acba0d870d74d0403
Olya Y <f_akora@mail.ru>
2022-10-17 23:03:59 +0300

    feat: do add method!

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -15,7 +15,20 @@ class BinarySearchTree {
   }
 
   add(data) {
+    this.rooty = addData(this.rooty, data);
 
+    function addData(node, data){
+      if (!node){
+        return new Node(data);
+      } else if (node.data == data) {
+        return node;
+      } else if (data < node.data){
+        node.left = addData(node.left, data);
+      } else if (data > node.data){
+        node.right = addData(node.right, data);
+      }
+      return node;
+    }
   }
 
   has(data) {
